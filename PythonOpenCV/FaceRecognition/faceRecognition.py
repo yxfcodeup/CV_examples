4d3233dc379bb4f65358cb069c227c1c1e2c7398
yxfcodeup <825378343@qq.com>
2017-06-20 15:15:43 +0800

    update

--- PythonOpenCV/FaceRecognition/faceRecognition.py
+++ PythonOpenCV/FaceRecognition/faceRecognition.py
@@ -11,15 +11,10 @@ from matplotlib import pyplot as plt
 haarcascade_frontalface_default.xml是opencv针对人脸已经训练好的模型数据
 """
 
-img1 = cv2.imread("./ScarlettJohansson.jpg")
+img1 = cv2.imread("./vsmodel2.jpg")
 gray_img = cv2.cvtColor(img1 , cv2.COLOR_BGR2GRAY)
 face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
-faces = face_cascade.detectMultiScale(
-        gray_img , 
-        scaleFactor = 1.15 ,
-        minNeighbors = 5 , 
-        minSize = (5 , 5)
-        )
+faces = face_cascade.detectMultiScale(gray_img , scaleFactor = 1.1 , minNeighbors = 3 , minSize = (5 , 5))
 
 for (x , y , w , h) in faces :
     cv2.rectangle(gray_img , (x , y) , (x+w , y+h) , (0 , 255 , 0) , 2)
