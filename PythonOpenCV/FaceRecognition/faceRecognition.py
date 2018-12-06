0f30ac99669d011b2389196d3b227a818aa14403
PloyoWiself <yxfylk9765@163.com>
2018-12-06 10:31:00 +0800

    update

--- PythonOpenCV/FaceRecognition/faceRecognition.py
+++ PythonOpenCV/FaceRecognition/faceRecognition.py
@@ -1,6 +1,6 @@
 import os
 import sys
-#External Libs
+# External Libs
 import cv2
 import numpy as np
 from PIL import Image
@@ -11,8 +11,9 @@ from matplotlib import pyplot as plt
 haarcascade_frontalface_default.xml是opencv针对人脸已经训练好的模型数据
 """
 
-img1 = cv2.imread("./vsmodel2.jpg")
+img1 = cv2.imread("vsmodel1.jpg")
 gray_img = cv2.cvtColor(img1 , cv2.COLOR_BGR2GRAY)
+# cv2.cvtColor(img1 , gray_img ,  cv2.COLOR_BGR2GRAY)
 face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 faces = face_cascade.detectMultiScale(gray_img , scaleFactor = 1.1 , minNeighbors = 3 , minSize = (5 , 5))
 
